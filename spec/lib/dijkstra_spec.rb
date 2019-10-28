fb5001112702513d84035d7ef69e5712c3721dca
RodolphoCLobo <rodolpho1704@gmail.com>
2019-10-27 22:18:47 -0300

    Feat: create tests for Dijkstra.

---
+++ spec/lib/dijkstra_spec.rb
@@ -0,0 +1,29 @@
+require './spec/spec_helper'
+
+RSpec.describe Dijkstra, type: :lib do
+
+  let!(:d1) { create(:distance_1) }
+  let!(:d2) { create(:distance_2) }
+  let!(:d3) { create(:distance_3) }
+
+  describe 'class methods' do
+    describe '#new' do
+      context 'when try create object whith valid params.' do
+
+        it 'should return an object containing the resolved graph.' do
+          distances = Distance.all
+          edges = ApplicationHelper.parse_edges(distances)
+
+          expect(Dijkstra.new('a', 'c', edges).graph.empty?).to eq(false)
+        end
+
+        it 'should return an instance from Dijkstra class.' do
+          distances = Distance.all
+          edges = ApplicationHelper.parse_edges(distances)
+
+          expect(Dijkstra.new('a', 'c', edges).class).to eq(Dijkstra)
+        end
+      end
+    end
+  end
+end
